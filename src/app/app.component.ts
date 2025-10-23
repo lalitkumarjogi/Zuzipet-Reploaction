cfcb429c88c66b009eeba2d6d2a84c4a3fdfdd61
Lalit Kumar <lalitkumarjogi@gmail.com>
2025-10-23 21:17:45 +0530

    UI Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Petrelocation';
+   isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
    menuOpen = false;
 
   toggleMenu() {
